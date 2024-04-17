7073630da01e6cb069c2a1e5c198d991bd0b727e
kylehoang92 <kyle.hoang@techbank-software.com>
2024-04-17 15:09:37 +0700

    chore: support more networks

--- src/main.tsx
+++ src/main.tsx
@@ -9,22 +9,47 @@ import { WagmiProvider } from 'wagmi';
 import { Chain } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-const carbon = {
-  id: 9791,
-  name: 'Carbon EVM Devnet',
+const carbonTestnet = {
+  id: 9792,
+  name: 'Carbon EVM Testnet',
   nativeCurrency: { name: 'Carbon', symbol: 'SWTH', decimals: 18 },
   rpcUrls: {
-    default: { http: ['https://evm-scan.carbon.network'] },
+    default: { http: ['https://test-evm-api.carbon.network/'] },
   },
   blockExplorers: {
     default: { name: 'carbonscan', url: 'https://evm-scan.carbon.network' },
   },
 } as const satisfies Chain;
 
+const carbonDevnet = {
+  id: 9791,
+  name: 'Carbon EVM Devnet',
+  nativeCurrency: { name: 'Carbon', symbol: 'SWTH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://dev-evm-api.carbon.network/'] },
+  },
+  blockExplorers: {
+    default: { name: 'carbonscan', url: 'https://evm-scan.carbon.network' },
+  },
+} as const satisfies Chain;
+
+const carbon = {
+  id: 9790,
+  name: 'Carbon EVM',
+  nativeCurrency: { name: 'Carbon', symbol: 'SWTH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://evm-api.carbon.network/'] },
+  },
+  blockExplorers: {
+    default: { name: 'carbonscan', url: 'https://evm-scan.carbon.network' },
+  },
+} as const satisfies Chain;
+
+
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [carbon],
+  chains: [carbon, carbonDevnet, carbonTestnet],
   ssr: false,
 });
 
